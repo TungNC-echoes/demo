bef84ef19a12008b2000d024757a5ceb339f85be
TungDEPZAI <tungnc@kaopiz.com>
2019-06-11 17:39:40 +0700

    -a

--- index.php
+++ index.php
@@ -6,200 +6,85 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="fabric.js"></script>
     <style>
-        body {
-            font-family: Arial;
-            margin: 0;
-        }
-
-        * {
-            box-sizing: border-box;
-        }
-
-        img {
-            vertical-align: middle;
-        }
-
-        /* Position the image container (needed to position the left and right arrows) */
-        .container {
-            position: relative;
-            width: 50%;
-        }
-
-        /* Hide the images by default */
-        .mySlides {
-            display: none;
-        }
-
-        /* Add a pointer when hovering over the thumbnail images */
-        .cursor {
-            cursor: pointer;
-        }
-
-        /* Next & previous buttons */
-        .prev,
-        .next {
-            cursor: pointer;
-            position: absolute;
-            top: 40%;
-            width: auto;
-            padding: 16px;
-            margin-top: -50px;
-            color: white;
-            font-weight: bold;
-            font-size: 20px;
-            border-radius: 0 3px 3px 0;
-            user-select: none;
-            -webkit-user-select: none;
-        }
-
-        /* Position the "next button" to the right */
-        .next {
-            right: 0;
-            border-radius: 3px 0 0 3px;
-        }
-
-        /* On hover, add a black background color with a little bit see-through */
-        .prev:hover,
-        .next:hover {
-            background-color: rgba(0, 0, 0, 0.8);
-        }
-
-        /* Number text (1/3 etc) */
-        .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
-        }
-
-        /* Container for image text */
-        .caption-container {
-            text-align: center;
-            background-color: #222;
-            padding: 2px 16px;
-            color: white;
-        }
-
-        .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
-        /* Six columns side by side */
-        .column {
-            float: left;
-            width: 16.66%;
-        }
-
-        /* Add a transparency effect for thumnbail images */
-        .demo {
-            opacity: 0.6;
-        }
-
-        .active,
-        .demo:hover {
-            opacity: 1;
+        canvas {
+            border: 1px solid black;
         }
     </style>
     <title>DEMO</title>
 </head>
 <body>
-
-<h2 style="text-align:center">Slideshow Gallery</h2>
-
-<div class="container">
-    <div class="mySlides" id="img1">
-        <div class="numbertext">1 / 6</div>
-        <img src="img_woods.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides" id="img2">
-        <div class="numbertext">2 / 6</div>
-        <img src="img_5terre.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides" id="img3">
-        <div class="numbertext">3 / 6</div>
-        <img src="img_mountains.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides" id="img4">
-        <div class="numbertext">4 / 6</div>
-        <img src="img_lights.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides" id="img5">
-        <div class="numbertext">5 / 6</div>
-        <img src="img_nature.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides" id="img6">
-        <div class="numbertext">6 / 6</div>
-        <img src="img_snow.jpg" style="width:100%">
-    </div>
-
-    <a class="prev" onclick="plusSlides(-1)">❮</a>
-    <a class="next" onclick="plusSlides(1)">❯</a>
-
-    <div class="caption-container">
-        <p id="caption"></p>
-    </div>
-
-    <div class="row">
-        <div class="column">
-            <img class="demo cursor" src="img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-        </div>
-        <div class="column">
-            <img class="demo cursor" src="img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-        </div>
-        <div class="column">
-            <img class="demo cursor" src="img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-        </div>
-        <div class="column">
-            <img class="demo cursor" src="img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-        </div>
-        <div class="column">
-            <img class="demo cursor" src="img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
-        </div>
-        <div class="column">
-            <img class="demo cursor" src="img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
-        </div>
-    </div>
-</div>
-
+<input type="file" id="file">
+<input type="color" value="blue" id="fill"/>
+<input type="font-size" value="blue" id="fill"/>
+<select id="font">
+    <option>arial</option>
+    <option>tahoma</option>
+    <option>times new roman</option>
+</select>
+<br/>
+<canvas id="canvas" width="750" height="550"></canvas>
+<br/>
 <script>
-    var slideIndex = 1;
-    showSlides(slideIndex);
+    var canvas = new fabric.Canvas('canvas', {
+        backgroundImage: 'img_lights.jpg',
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+    });
+    fabric.Image.fromURL('img_lights.jpg', function (oImg) {
+        canvas.add(oImg);
+    });
+    document.getElementById('file').addEventListener("change", function (e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        console.log("reader   " + reader);
+        reader.onload = function (f) {
+            var data = f.target.result;
+            fabric.Image.fromURL(data, function (img) {
+                var oImg = img.set({ width: 750, height: 500, angle: 0}).scale(0.9);
+                canvas.add(oImg).renderAll();
+                canvas.setActiveObject(oImg);
+            });
+        };
+        reader.readAsDataURL(file);
+    });
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+    $('#fill').change(function () {
+        var obj = canvas.getActiveObject();
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("demo");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+        if (obj) {
+            obj.set("fill", this.value);
         }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
+        canvas.renderAll();
+    });
+
+    $('#font').change(function () {
+        var obj = canvas.getActiveObject();
+
+        if (obj) {
+            obj.set("fontFamily", this.value);
         }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
-        captionText.innerHTML = dots[slideIndex-1].alt;
+
+        canvas.renderAll();
+    });
+
+    function addText(e) {
+        var oText = new fabric.IText('Tap and Type', {
+            top:e.offsetY,
+            cursorDuration:200,
+            left:e.offsetX,
+        });
+
+        canvas.add(oText);
+        oText.bringToFront();
+        canvas.setActiveObject(oText);
+        $('#fill, #font').trigger('change');
     }
+    canvas.on('mouse:down', function(options) {
+        if (options.target == null)
+            addText(options.e);
+    });
 </script>
 
 </body>
